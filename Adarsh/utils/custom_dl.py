f59bea74a3a3738cc1b6e6f05aeb4fd8611b621f
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-01-29 21:08:58 +0530

    update Adarsh/utils/custom_dl.py

--- Adarsh/utils/custom_dl.py
+++ Adarsh/utils/custom_dl.py
@@ -1,14 +1,14 @@
 import math
 import asyncio
 import logging
-from Adarsh.vars import Var
+from Dxbots.vars import Var
 from typing import Dict, Union
-from Adarsh.bot import work_loads
+from Dxbots.bot import work_loads
 from pyrogram import Client, utils, raw
 from .file_properties import get_file_ids
 from pyrogram.session import Session, Auth
 from pyrogram.errors import AuthBytesInvalid
-from Adarsh.server.exceptions import FIleNotFound
+from Dxbots.server.exceptions import FIleNotFound
 from pyrogram.file_id import FileId, FileType, ThumbnailSource
 
 
