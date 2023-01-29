405d167196f7a35dd5867acdec5622f3afc16e63
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-01-30 00:49:54 +0530

    Add files via upload

---
+++ Dxbots/bot/__init__.py
@@ -0,0 +1,16 @@
+from pyrogram import Client
+import pyromod.listen
+from ..vars import Var
+from os import getcwd
+
+DxStreamBot = Client(
+    name='Web Streamer',
+    api_id=Var.API_ID,
+    api_hash=Var.API_HASH,
+    bot_token=Var.BOT_TOKEN,
+    sleep_threshold=Var.SLEEP_THRESHOLD,
+    workers=Var.WORKERS
+)
+
+multi_clients = {}
+work_loads = {}
