ce4d5001ff00b4315178d4730aad004e7bda097f
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-02-04 21:02:13 +0530

    Fixed✅

--- Dxbots/bot/plugins/stream.py
+++ Dxbots/bot/plugins/stream.py
@@ -106,7 +106,7 @@ async def private_receive_handler(c: Client, m: Message):
             quote=True,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("STREAM🖥", url=stream_link), #Stream Link
-                                                InlineKeyboardButton('DOWNLOAD📥', url=online_link)]]) #Download Link
+                                                InlineKeyboardButton("DOWNLOAD📥", url=online_link)]]) #Download Link
         )
     except FloodWait as e:
         print(f"Sleeping for {str(e.x)}s")
