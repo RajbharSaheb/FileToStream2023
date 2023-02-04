69e37deb10f9564059c97c79942e667df5986e4b
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-02-04 20:53:14 +0530

    Fix✅
    Fixed button space for new telegram api✅

--- Dxbots/bot/plugins/stream.py
+++ Dxbots/bot/plugins/stream.py
@@ -105,8 +105,8 @@ async def private_receive_handler(c: Client, m: Message):
             text=msg_text.format(get_name(log_msg), humanbytes(get_media_file_size(m)), online_link, stream_link),
             quote=True,
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("STREAM 🖥", url=stream_link), #Stream Link
-                                                InlineKeyboardButton('DOWNLOAD 📥', url=online_link)]]) #Download Link
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("STREAM🖥", url=stream_link), #Stream Link
+                                                InlineKeyboardButton('DOWNLOAD📥', url=online_link)]]) #Download Link
         )
     except FloodWait as e:
         print(f"Sleeping for {str(e.x)}s")
