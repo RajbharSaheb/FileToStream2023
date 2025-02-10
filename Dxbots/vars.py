51f9e5ee3364b08f60a4ee6cf3b84d38fd6092a5
Ksjsjjsksksisjsnsnsnzzbzysyshshshjsjsj <koushalprasad35@gmail.com>
2025-02-10 15:51:30 +0530

    Update vars.py

--- Dxbots/vars.py
+++ Dxbots/vars.py
@@ -8,20 +8,20 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID'))
-    API_HASH = str(getenv('API_HASH'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN'))
+    API_ID = int(getenv('API_ID', '29534418'))
+    API_HASH = str(getenv('API_HASH', '5f15dd792990ade40a43ae17413b422f'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '7863724997:AAFOoZxvCn765LeWhG1GSHkzKMAvVUbd4jo'))
     name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('-1001930913574'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL','-1002161128126'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5838674798").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5691486059").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('rajbharsaheb'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'dm_me_br'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
@@ -36,6 +36,6 @@ class Var(object):
     else:
         #URL = "https://{}/".format(FQDN)
         URL = FQDN 
-    DATABASE_URL = str(getenv('mongodb+srv://urluploader:urluploader@cluster0.uzzcbud.mongodb.net/?retryWrites=true&w=majority'))
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://fffworld57:Or97XLuv6P6GvtFZ@cluster0.u34tcer.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'))
     UPDATES_CHANNEL = str(getenv('-1001966099561', None))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
